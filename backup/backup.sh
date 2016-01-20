894688547a91c5babdc29417701c40baab8fef2f
Coy Humphrey <coyizumi@gmail.com>
2016-01-19 19:07:13 -0800

    Updated backup script with link to description

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# https://wiki.archlinux.org/index.php/full_system_backup_with_rsync
 rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /path/to/backup/folder
