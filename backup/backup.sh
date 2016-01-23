1b04f25df9a26e771713f7c2179cfab88ef92519
Coy Humphrey <cmhumphr@ucsc.edu>
2016-01-23 11:45:51 -0800

    Updated backup.sh to point to /mnt/backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://wiki.archlinux.org/index.php/full_system_backup_with_rsync
 # Options of interest: --numeric-ids --checksum
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /path/to/backup/folder
+rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /mnt/backup
