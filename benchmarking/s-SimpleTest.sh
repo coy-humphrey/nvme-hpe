951c3a0cc962947e2e650e0edfdfe3df612d7661
Jayden Navarro <jdndeveloper@gmail.com>
2016-02-27 14:11:08 -0800

    Changes to the benchmark script

--- benchmarking/s-SimpleTest.sh
+++ benchmarking/s-SimpleTest.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ib_send_bw tnvme40Gp1 --report_gbits
+OUTFILE="results/ib_send_results.txt"
+INTF="tnvme40Gp1"
+IB_SEND_BW="ib_send_bw $INTF --report_gbits"
+
+echo "" > $OUTFILE
+
+COUNTER=1
+while [  $COUNTER -lt 5 ]; do
+    $( $IB_SEND_BW 2>&1 | grep "Gb/s" -A 1 &>> $OUTFILE )
+
+    let COUNTER=COUNTER+1 
+done
