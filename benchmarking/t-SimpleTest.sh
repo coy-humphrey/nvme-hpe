aaea627b31dc88b7fc9bd186fd70f615d7d53165
Jayden Navarro <jdndeveloper@gmail.com>
2016-02-27 14:32:45 -0800

    Modified t-SimpleTest.sh

--- benchmarking/t-SimpleTest.sh
+++ benchmarking/t-SimpleTest.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ib_send_bw --report_gbits
+IB_SEND_BW="ib_send_bw --report_gbits"
+
+COUNTER=1
+while [ $COUNTER -lt 5  ]; do
+    $IB_SEND_BW &> /dev/null
+
+    let COUNTER=COUNTER+1
+done
