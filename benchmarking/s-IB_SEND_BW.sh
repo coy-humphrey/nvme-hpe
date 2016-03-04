f66d8673823a11396ad83f51f6539868edd38991
Jayden Navarro <jdndeveloper@gmail.com>
2016-03-03 18:41:27 -0800

    Added run all script and polished the IB_SEND_BW script

--- benchmarking/s-IB_SEND_BW.sh
+++ benchmarking/s-IB_SEND_BW.sh
@@ -7,18 +7,16 @@ OUTFILE="results/ib_send_results.txt"
 INTF="tnvme40Gp1"
 MAIN_COMMAND="ib_send_bw $INTF --report_gbits"
 
-
 KEYWORD_1="Gb/s"
 KEYWORD_2="\"Send BW Test\""
 GREP_0="grep $KEYWORD_2 -B 1 -A 19"
 GREP_1="grep $KEYWORD_1 -A 1"
 GREP_2="$GREP_1 | grep $KEYWORD_1 -v"
 
-OUT_COMMAND_PRELUDE="$IB_SEND_BW 2>&1 | $GREP_0 &>> $OUTFILE"
-OUT_COMMAND="$IB_SEND_BW 2>&1 | $GREP_2 &>> $OUTFILE"
+OUT_COMMAND_PRELUDE="$MAIN_COMMAND 2>&1 | $GREP_0 &>> $OUTFILE"
+OUT_COMMAND="$MAIN_COMMAND 2>&1 | $GREP_2 &>> $OUTFILE"
 
 NUM_RUNS=20
-
 
 # END of variables to modify
 
