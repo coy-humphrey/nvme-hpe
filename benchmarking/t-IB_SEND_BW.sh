fc41a49aeb2dd2677d171c3626ea48bcdd48986b
Jayden Navarro <jdndeveloper@gmail.com>
2016-03-03 18:24:10 -0800

    Modified IB_SEND_BW.sh and renamed it

--- benchmarking/t-IB_SEND_BW.sh
+++ benchmarking/t-IB_SEND_BW.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# START of variables to modify
+
 IB_SEND_BW="ib_send_bw --report_gbits"
 
+NUM_RUNS=20
+
+# END of variables to modify
+
 COUNTER=0
-UPPER_BOUND=20
-while [ $COUNTER -lt $UPPER_BOUND ]; do
+while [ $COUNTER -lt $NUM_RUNS ]; do
     $IB_SEND_BW &> /dev/null
 
     let COUNTER=COUNTER+1
