bba23caa64ff1702df1fa67a05bce3f3b2e44e02
Alice Yu <aljyu@snvme.soe.ucsc.edu>
2016-04-08 13:11:45 -0700

    Added more code to nft.c

--- benchmarking/nft/nft.c
+++ benchmarking/nft/nft.c
@@ -25,6 +25,12 @@
 #define FAILURE 1 
 #define FALSE 0
 #define TRUE 1
+#define ASCII_LOW 0x20
+#define ASCII_HIGH 0x7E
+#define BINARY_LOW 0x00
+#define BINARY_HIGH 0xFF
+#define BINARY 0
+#define TEXT 1
 
 /* void usage()
  * Print usage and exit with a failure
@@ -33,6 +39,29 @@ void usage(){
    fprintf(stdout, "Usage: \n");
    fprintf(stdout, "       nft -h \n");
    exit(FAILURE);
+}
+
+/* 
+ *
+ *
+ */
+void randFill(char * buffer, int bufferSize, int type) {
+  int randVal, i;
+  buffer = malloc(bufferSize);
+  if (buffer == NULL) {
+    fprintf(stderr, "Failed to allocate memory or bufferSize is 0");
+    exit(FAILURE);
+  }
+  // default for type is a binary file
+  for (i = 0; i < bufferSize; i++) {
+    if (type) {
+      randVal = (random() & BINARY_HIGH);
+    }
+    else {
+      randVal = ASCII_LOW + (random() % (ASCII_HIGH - ASCII_LOW));
+    }
+    buffer[i] = randVal;
+  }
 }
 
 /* int main(int argc, char * argv[])
@@ -49,6 +78,7 @@ int main(int argc, char * argv[]){
    int checkSumFlag = TRUE; 
    int verboseFlag = FALSE;
    int debugFlag = FALSE;
+   int fileTypeFlag = BINARY;
 
    // set program name
    programName = basename(argv[0]);
@@ -57,7 +87,7 @@ int main(int argc, char * argv[]){
    // as needed. On exit of the loop, the optind will point to the
    // file passed, if there was one passed.
    //while ((currentOption = getopt(argc, argv, "ly@:D:"))!= -1){
-   while ((currentOption = getopt(argc, argv, "hcvdb:f:t:n:s:"))!= -1){
+   while ((currentOption = getopt(argc, argv, "hcvds:b:f:t:n:"))!= -1){
       switch(currentOption){
          case 'h':
             usage();
@@ -74,6 +104,10 @@ int main(int argc, char * argv[]){
             fprintf(stdout, "option -d passed \n");
             debugFlag = TRUE;
             break;
+         case 's':
+            fprintf(stdout, "option -s passed with flag [%s]\n");
+            fileTypeFlag = TEXT;
+            break;
 
          case 'b':
             fprintf(stdout, "option -b passed with flag [%s]\n", optarg);
@@ -86,9 +120,6 @@ int main(int argc, char * argv[]){
             break;
          case 'n':
             fprintf(stdout, "option -n passed with flag [%s]\n", optarg);
-            break;
-         case 's':
-            fprintf(stdout, "option -s passed with flag [%s]\n", optarg);
             break;
 
          case ':':
