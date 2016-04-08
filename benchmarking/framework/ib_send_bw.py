771cce392d3d96b580590dc5998f2b29656cf6d9
Jayden Navarro <jdndeveloper@gmail.com>
2016-04-08 11:09:42 -0700

    Made code more pythonic

--- benchmarking/framework/ib_send_bw.py
+++ benchmarking/framework/ib_send_bw.py
@@ -5,21 +5,18 @@
 import sys
 from framework import Framework
 
-outputHeaders_t = ['Gb/s']
-outputHeaders_s = ['Gb/s']
-
 def run_t():
    framework = Framework()
    
    framework.command = "ib_send_bw"
    framework.numRuns = 3
    framework.isClient = False
-   framework.outputParser = parse_t
+   framework.outputParser = parse
    framework.waitTime = 0.5
    framework.headerNames = ['Duration (-D)', \
                             'TxDepth (-t)', \
                             'Report GBits (--report_gbits)']
-   framework.outputHeaders = outputHeaders_t
+   framework.outputHeaders = ['Gb/s']
    framework.params = [
       ('-D', ['1', '3', '5']),
       ('-t', ['256', '512']),
@@ -28,18 +25,21 @@ def run_t():
    ]
    framework.runBenchmarks()
 
+   def parse(output):
+      return {framework.outputHeaders[0]: float(output)}
+
 def run_s():
    framework = Framework()
    
    framework.command = "ib_send_bw"
    framework.numRuns = 3
    framework.isClient = True
-   framework.outputParser = parse_s
+   framework.outputParser = parse
    framework.headerNames = ['Port (tnvme40Gp1)', \
                             'Duration (-D)', \
                             'TxDepth (-t)', \
                             'Report GBits (--report_gbits)']
-   framework.outputHeaders = outputHeaders_s
+   framework.outputHeaders = ['Gb/s']
    framework.params = [
       ('tnvme40Gp1', ['']),
       ('-D', ['1', '3', '5']),
@@ -49,11 +49,9 @@ def run_s():
    ]
    framework.runBenchmarks() 
 
-def parse_t(output):
-   return {outputHeaders_t[0]: float(output)}
+   def parse(output):
+      return {framework.outputHeaders[0]: float(output)}
 
-def parse_s(output):
-   return {outputHeaders_t[0]: float(output)}
 
 if __name__ == "__main__":
    if sys.argv[1] == '-t':
