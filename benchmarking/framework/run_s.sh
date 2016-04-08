b950521ef3c958c0224ae89dc29f041973ff345d
Jayden Navarro <jdndeveloper@gmail.com>
2016-04-08 11:26:31 -0700

    Added automatic moving of error file

--- benchmarking/framework/run_s.sh
+++ benchmarking/framework/run_s.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 python run_all_s.py 2> errors/stderr-s.txt
+DIR=`ls -d errors/*/ | cat | tail -n 1`
+mv errors/stderr-s.txt "$DIR"
--- benchmarking/framework/run_t.sh
+++ benchmarking/framework/run_t.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 python run_all_t.py 2> errors/stderr-t.txt
+DIR=`ls -d errors/*/ | cat | tail -n 1`
+mv errors/stderr-t.txt "$DIR"
